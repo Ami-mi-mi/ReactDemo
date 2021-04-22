70f6f5e86f1b2023dbd14812b0e11d88c2087a58
aixl <aixl@channelsoft.com>
2021-04-22 09:59:56 +0800

    1、创建CCOD业务申请页面

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,12 @@
 
 import myOrder from '@Views/main/components/myOrder/myOrder'
 import resourceApply from '@Views/main/components/acceptOrderManage/resourceApply/resourceApply'
+import ccodBusinessApply from '@Views/main/components/acceptOrderManage/ccodBusinessApply/ccodBusinessApply.js'
 
 const routes = {
     '/my-order': myOrder,
-    '/resource-apply': resourceApply
+    '/resource-apply': resourceApply,
+    '/ccod-business-apply': ccodBusinessApply
 };
 
 export default routes;
---
+++ src/views/main/components/acceptOrderManage/ccodBusinessApply/ccodBusinessApply.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ccodBusinessApply extends React.Component {
+    render() {
+        return (<div>ccod业务申请</div>)
+    }
+}
+
+export default ccodBusinessApply;
