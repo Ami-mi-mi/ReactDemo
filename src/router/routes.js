4dd68273676fe291c212ad7d1225bef0aafde5db
aixl <aixl@channelsoft.com>
2021-04-22 10:04:39 +0800

    添加ccod业务调账页面

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,14 @@
 
 import myOrder from '@Views/main/components/myOrder/myOrder'
 import resourceApply from '@Views/main/components/acceptOrderManage/resourceApply/resourceApply'
-import ccodBusinessApply from '@Views/main/components/acceptOrderManage/ccodBusinessApply/ccodBusinessApply.js'
+import ccodBusinessApply from '@Views/main/components/acceptOrderManage/ccodBusinessApply/ccodBusinessApply'
+import ccodAccoutAdjust from '@Views/main/components/acceptOrderManage/ccodAccoutAdjust/ccodAccoutAdjust'
 
 const routes = {
     '/my-order': myOrder,
     '/resource-apply': resourceApply,
-    '/ccod-business-apply': ccodBusinessApply
+    '/ccod-business-apply': ccodBusinessApply,
+    '/ccod-account-adjust': ccodAccoutAdjust
 };
 
 export default routes;
---
+++ src/views/main/components/acceptOrderManage/ccodAccoutAdjust/ccodAccoutAdjust.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ccodAccountAdjust extends React.Component {
+    render() {
+        return (<div>ccod业务调账</div>)
+    }
+}
+
+export default ccodAccountAdjust;
