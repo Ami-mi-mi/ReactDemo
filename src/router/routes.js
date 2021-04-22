71f7ef86ba37cccbaef175dc4bb4ea2e30b63c1d
aixl <aixl@channelsoft.com>
2021-04-22 10:15:18 +0800

    添加对账管理页面

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,13 +6,15 @@ import ccodBusinessApply from '@Views/main/components/acceptOrderManage/ccodBusi
 import ccodAccoutAdjust from '@Views/main/components/acceptOrderManage/ccodAccoutAdjust/ccodAccoutAdjust'
 //结算工单
 import bottomTicketManage from '@Views/main/components/settlementOrderManage/bottomTicketManage/bottomTicketManage'
+import balanceAccountManage from '@Views/main/components/settlementOrderManage/balanceAccountManage/balanceAccountManage'
 
 const routes = {
     '/my-order': myOrder,
     '/resource-apply': resourceApply,
     '/ccod-business-apply': ccodBusinessApply,
     '/ccod-account-adjust': ccodAccoutAdjust,
-    '/settlement-order-manage/bottom-ticket-manage': bottomTicketManage
+    '/settlement-order-manage/bottom-ticket-manage': bottomTicketManage,
+    '/settlement-order-manage/balance-account-manage': balanceAccountManage
 };
 
 export default routes;
---
+++ src/views/main/components/settlementOrderManage/balanceAccountManage/balanceAccountManage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class balanceAccountManage extends React.Component {
+    render() {
+        return (<div>ccod业务申请</div>)
+    }
+}
+
+export default balanceAccountManage;
