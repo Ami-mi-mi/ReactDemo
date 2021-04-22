3d26a506d68872caf9f78c4220788b13a42cc16c
aixl <aixl@channelsoft.com>
2021-04-22 10:12:11 +0800

    添加开票底表页面

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,18 @@
-
+//我的工单
 import myOrder from '@Views/main/components/myOrder/myOrder'
+//受理工单
 import resourceApply from '@Views/main/components/acceptOrderManage/resourceApply/resourceApply'
 import ccodBusinessApply from '@Views/main/components/acceptOrderManage/ccodBusinessApply/ccodBusinessApply'
 import ccodAccoutAdjust from '@Views/main/components/acceptOrderManage/ccodAccoutAdjust/ccodAccoutAdjust'
+//结算工单
+import bottomTicketManage from '@Views/main/components/settlementOrderManage/bottomTicketManage/bottomTicketManage'
 
 const routes = {
     '/my-order': myOrder,
     '/resource-apply': resourceApply,
     '/ccod-business-apply': ccodBusinessApply,
-    '/ccod-account-adjust': ccodAccoutAdjust
+    '/ccod-account-adjust': ccodAccoutAdjust,
+    '/settlement-order-manage/bottom-ticket-manage': bottomTicketManage
 };
 
 export default routes;
---
+++ src/views/main/components/settlementOrderManage/bottomTicketManage/bottomTicketManage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class bottomTicketManage extends React.Component {
+    render() {
+        return (<div>开票底表</div>)
+    }
+}
+
+export default bottomTicketManage;
